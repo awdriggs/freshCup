9116f83fa5050f90ca64a083ba226b602889d844
adam driggers <awdriggs@gmail.com>
2017-02-24 18:58:03 -0500

    first push

---
+++ app.js
@@ -0,0 +1,56 @@
+// coffee data model
+// this should be coming from the server
+// right now a sample object to represent expected format
+
+var apiData = {
+  coffees: [
+    {
+      title: "Nyamasheke",
+      orgin: "Rwanda",
+      producer: "Nyungwe Cooperative",
+      body: "Full",
+      flavor: ["caramel", "cocoa", "mulling spice"],
+      adjectives: ["balanced", "complex", "bright"],
+      notes: "Good for espresso"
+    },
+    {
+      title: "Agora",
+      orgin: "Ethiopia",
+      producer: "Duromina Cooperative",
+      body: "syrupy",
+      flavor: ["peach", "mango", "lemon", "caramel", "bergamot", "layered chocolate", "dark fruits"],
+      adjectives: ["clean", "tart", "juicy"],
+      notes: "Good for espresso"
+    },
+  ] // end of coffee array
+}
+
+
+// components
+//coffee-item, view for showing each individual coffee item
+Vue.component('coffee-item', {
+  template: '<div> {{ coffee.title }}, {{ coffee.orgin }} </div>',
+  props: ['coffee']
+})
+  
+// all, show all coffees in the list
+Vue.component('all-view', {
+  template: '<div is="coffee-item" v-for="coffee in coffees" v-bind:coffee="coffee"></div>',
+  props: ['coffees']
+})
+
+// new, create a new coffee
+  //  sub components
+  //  add body
+//    add flavors
+//    add intesifiers
+//    add notes
+
+
+var vm = new Vue({
+  el: '#main',
+  data: {
+    coffees: apiData.coffees,
+    message: 'Hello World!'
+  }, 
+})
