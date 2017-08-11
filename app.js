9ec719e6eccce60a5be6b7c7400be2eafbf5e95f
adam driggers <awdriggs@gmail.com>
2017-08-10 22:12:35 -0400

    refactor after reading book

--- app.js
+++ app.js
@@ -3,50 +3,86 @@
 // right now a sample object to represent expected format
 
 var apiData = {
-  coffees: [
-    {
-      title: "Nyamasheke",
-      orgin: "Rwanda",
-      producer: "Nyungwe Cooperative",
-      body: "Full",
-      flavor: ["caramel", "cocoa", "mulling spice"],
-      adjectives: ["balanced", "complex", "bright"],
-      notes: "Good for espresso"
+  coffees: [{
+    title: "Nyamasheke",
+    origin: "Rwanda",
+    producer: "Nyungwe Cooperative",
+    body: "Full",
+    flavor: ["caramel", "cocoa", "mulling spice"],
+    adjectives: ["balanced", "complex", "bright"],
+    notes: "Good for espresso"
+  }, {
+    title: "Agora",
+    origin: "Ethiopia",
+    producer: "Duromina Cooperative",
+    body: "syrupy",
+    flavor: ["peach", "mango", "lemon", "caramel", "bergamot", "layered chocolate", "dark fruits"],
+    adjectives: ["clean", "tart", "juicy"],
+    notes: "Good for espresso"
+  }, ], // end of coffee array
+  flavors: [{
+      category: "fruit",
+      notes: ['lemon', 'lime', 'grapefruit', 'clementine', 'tangerine', 'mandarin orange', 'orange', ]
+    }, {
+      category: "chocolate",
+      notes: ['cacao nibs', 'dark chocolate', 'bakers chocalate', 'bitterwsweet chocolate', 'cocoa powder', 'milk chocolate']
     },
-    {
-      title: "Agora",
-      orgin: "Ethiopia",
-      producer: "Duromina Cooperative",
-      body: "syrupy",
-      flavor: ["peach", "mango", "lemon", "caramel", "bergamot", "layered chocolate", "dark fruits"],
-      adjectives: ["clean", "tart", "juicy"],
-      notes: "Good for espresso"
-    },
-  ] // end of coffee array
+
+  ]
 }
 
 
 // components
 //coffee-item, view for showing each individual coffee item
-Vue.component('coffee-item', {
-  template: '<div> {{ coffee.title }}, {{ coffee.orgin }} </div>',
+Vue.component('coffeeItem', {
+  template: "#coffee-item",
   props: ['coffee']
 })
-  
+
+// new coffee
+Vue.component('newCoffee', {
+  props: ['flavors', ],
+  template: "#new-coffee",
+  methods: {
+    reveal: function(flavor) {
+      this.current = flavor.notes;
+    }
+  },
+  data: function() {
+    return {
+      current: "",
+      flavorList: [],
+    }
+  }
+})
+
+Vue.component('flavor', {
+  props: ['note'],
+  template: "#flavor-btn",
+  methods: {
+    addFlavor: function(event) {
+      //emit the flavor..
+      //what is this context?
+      console.log('event', event);
+      console.log(this);
+    }
+  }
+})
+
 // all, show all coffees in the list
 
 // new, create a new coffee
-  //  sub components
-  //  add body
+//  sub components
+//  add body
 //    add flavors
 //    add intesifiers
 //    add notes
-
 
 var vm = new Vue({
   el: '#main',
   data: {
     coffees: apiData.coffees,
-    message: 'Hello World!'
-  }, 
+    flavors: apiData.flavors
+  },
+
 })
